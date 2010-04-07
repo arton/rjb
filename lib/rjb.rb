79aa5936e7ac11decb3c3b8679c46a3de7c758f2
arton <artonx@gmail.com>
2010-04-07 07:50:31 -0700

    add class_eval method for RJB's psuedo class.

--- lib/rjb.rb
+++ lib/rjb.rb
@@ -20,3 +20,34 @@ end
 
 require 'rjbcore'
 
+module Rjb
+  @@org_import = instance_method(:import)
+  def import(s)
+    o = @@org_import.bind(self).call(s)
+    o.instance_eval do
+      @user_initialize = nil
+      @org_new ||= method(:new)
+      @org_new_with_sig ||= method(:new_with_sig)
+      def new_with_sig(*args)
+        prepare_proxy(@org_new_with_sig(*args))
+      end
+      def new(*args)
+        prepare_proxy(@org_new.call(*args))
+      end
+      def class_eval(&proc)
+        @user_initialize = proc
+      end
+      private
+      def prepare_proxy(pxy)
+        pxy.instance_eval do
+          def include(*mod)
+            extend *mod
+          end
+        end  
+        pxy.instance_eval &@user_initialize if @user_initialize
+        pxy
+      end
+    end  
+    o
+  end
+end
