9e7b65d36c56a03be3fa95ec55b9d2e5f10edb15
arton <arton@nomad.(none)>
2010-10-24 19:32:47 +0900

    make sample script runnable

--- samples/filechooser.rb
+++ samples/filechooser.rb
@@ -1,46 +1,35 @@
+#!/usr/local/bin/ruby -Ks
+#coding: cp932
+
 require 'rjb'
 
 Rjb::load
 
-SwingUtilities = Rjb::import('javax.swing.SwingUtilities')
-JThread = Rjb::import('java.lang.Thread')
-JFileChooser = Rjb::import('javax.swing.JFileChooser')
-class Run
-  def initialize(&block)
-    @block = block
-  end
-  def run
-puts 'go-hello'
-    @block.call
-puts 'ret-hello'
+unless RUBY_VERSION =~ /^1\.9/
+  class String
+    def encode(s)
+      self
+    end
   end
 end
 
 class FileChooser
-  @@klass = JFileChooser
+  @@klass = Rjb::import('javax.swing.JFileChooser')
   def initialize(ext = '*', desc = 'any files')
     @selected = nil
   end
 
   def show()
-    run = Rjb::bind(Run.new do
-puts 'hello'
-		      @selected = nil
-		      chooser = @@klass.new()
-puts 'hello'
-		      ret = chooser.showOpenDialog(nil)
-puts 'hello'
-		      if ret == @@klass.APPROVE_OPTION 
-			@selected = chooser.getSelectedFile
-		      end
-		    end, 'java.lang.Runnable')
-    SwingUtilities.invokeAndWait(run)
+    chooser = @@klass.new
+    if chooser.showOpenDialog(nil) == @@klass.APPROVE_OPTION 
+      @selected = chooser.getSelectedFile
+    end
   end
   attr_reader :selected
 end
 
 f = FileChooser.new
-if f.show == 0
-  puts f.selected.getAbsolutePath
+if f.show  
+  puts f.selected.getAbsolutePath.encode('cp932')
 end
 puts 'bye'
